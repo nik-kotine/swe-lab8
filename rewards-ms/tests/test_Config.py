60bc10f934428c6c83e82d33148f39baddf7cfd4
nik-kotine <mswv280407@gmail.com>
2026-05-30 23:25:17 -0500

    chore: update sonar properties file

---
+++ rewards-ms/tests/test_Config.py
@@ -0,0 +1,6 @@
+import pytest
+from app.config import require_env_variable
+
+def test_RequireEnvVariableShoulRaiseErrorWhenMissing():
+    with pytest.raises(RuntimeError):
+        require_env_variable("THIS_VARIABLE_DOESNT_EXIST_676767")
--- rewards-ms/tests/test_RewardRepository.py
+++ rewards-ms/tests/test_RewardRepository.py
@@ -5,8 +5,10 @@ from app.domain.Reward import Reward
 def test_SaveRewardShouldSaveReward(tmp_path):
     db_file = tmp_path / "test.db"
     repository = SQLiteRewardRepository(db_path = db_file)
-    reward = Reward(user_id = "JOHNID", reward_points = 10)
-    repository.save_reward(reward)
+
+    reward_1 = Reward(user_id = "JOHNID", reward_points = 10)
+
+    repository.save_reward(reward_1)
     connection = sqlite3.connect(db_file)
     cursor = connection.cursor()
     cursor.execute("""SELECT user_id, reward_points FROM rewards WHERE user_id = ?""", ("JOHNID", ))
