b13eb0a7325d8151bb13e07a67bf0cf6699da421
nik-kotine <mswv280407@gmail.com>
2026-05-30 20:51:51 -0500

    fix: change test name prefixes

--- rewards-ms/tests/test_RewardCalculator.py
+++ rewards-ms/tests/test_RewardCalculator.py
@@ -1,15 +1,15 @@
 from ..app.domain.RewardCalculator import RewardCalculator
 import pytest
 
-def CalculatePositiveAmountShouldReturnValue():
+def test_CalculatePositiveAmountShouldReturnValue():
     calc = RewardCalculator()
     assert calc.calculate_reward(50) == 5
 
-def CalculateZeroAmountShouldReturnZero():
+def test_CalculateZeroAmountShouldReturnZero():
     calc = RewardCalculator()
     assert calc.calculate_reward(0) == 0
 
-def CalculateNegativeAmountShouldRaiseValueError():
+def test_CalculateNegativeAmountShouldRaiseValueError():
     calc = RewardCalculator()
     with pytest.raises(ValueError):
         calc.calculate_reward(-50)
