1ed0b52d45c41449c9d6e48d56ace16cce91c7d5
nik-kotine <mswv280407@gmail.com>
2026-05-30 22:18:36 -0500

    feat: add more tests

---
+++ rewards-ms/tests/queries/GetRewardFromUserId.sql
@@ -0,0 +1,1 @@
+SELECT user_id, reward_points FROM rewards WHERE user_id = ?
--- rewards-ms/tests/test_RewardRepository.py
+++ rewards-ms/tests/test_RewardRepository.py
@@ -1,3 +1,4 @@
+import sqlite3
 from app.adapters.SQLiteRewardRepository import SQLiteRewardRepository
 from app.domain.Reward import Reward
 
@@ -6,3 +7,13 @@ def test_SaveRewardShouldSaveReward(tmp_path):
     repository = SQLiteRewardRepository(db_path = db_file)
     reward = Reward(user_id = "JOHNID", reward_points = 10)
     repository.save_reward(reward)
+    connection = sqlite3.connect(db_file)
+    cursor = connection.cursor()
+    cursor.execute("""SELECT user_id, reward_points FROM rewards WHERE user_id = ?""", ("JOHNID", ))
+    row = cursor.fetchone()
+    connection.close()
+
+    assert row is not None
+    assert row[0] == "JOHNID"
+    assert row[1] == 10
+
